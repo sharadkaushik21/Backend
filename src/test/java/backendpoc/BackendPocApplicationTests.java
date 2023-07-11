9a43836fcdd4de824e6b133526f2bcdfa10ea50f
Sharad Kaushik <sksharadkaushik79@gmail.com>
2023-07-11 13:42:43 +0530

    First commit

---
+++ src/test/java/backendpoc/BackendPocApplicationTests.java
@@ -0,0 +1,13 @@
+package backendpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
